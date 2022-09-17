ee758cd5545b146cb35bac4a2725253d820c6178
CatW-M <wheelermushlin@gmail.com>
2022-09-17 12:37:40 -0700

    books.js: Added get route for home

--- controllers/books.js
+++ controllers/books.js
@@ -2,22 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
-router.get("/books", (req, res) => {
+router.get("/", (req, res) => {
   res.render("books/home");
+  console.log(`BOOKS!`)
 });
 
-router.get("/", (req, res) => {
-  res.render("auth/login");
-});
+
 //routes for individual book pages (discuss routes) if time allows
 
-//Logout Route
-router.get('/logout', (req, res) => {
-  req.logOut(() => {
-    console.log('I am logged out')
-  }); // logs the user out of the session
-  req.flash('success', 'Logging out... See you next time!');
-  res.redirect('/');
-});
+
 
 module.exports = router;
